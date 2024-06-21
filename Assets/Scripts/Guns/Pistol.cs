5f52594c6f21b69ae1204e24ef00abcb914091ee
Carl <94771147+BeetJuicer@users.noreply.github.com>
2024-06-21 18:01:31 +0800

    dash ability

--- Assets/Scripts/Guns/Pistol.cs
+++ Assets/Scripts/Guns/Pistol.cs
@@ -32,32 +32,8 @@ public class Pistol : PaintGun
 		// Cancel the charge if there is any, and shoot if allowed.
 		if (Input.GetButtonDown("Fire1"))
 		{
-			// cancel the charge count. disable charging unless the user actually lets go of the right mouse button.
-			isCharging = false;
-			chargeTimer = 0;
-
 			if (FireTimer >= actualROF && canFire)
 				Fire();
-		}
-
-		// start counting the charge if allowed.
-		if (Input.GetButtonDown("Fire2"))
-		{
-			isCharging = true;
-		}
-
-		// count the final charge time.
-		if (Input.GetButtonUp("Fire2"))
-		{
-			//calculate the charge level depending on the amount of time charged.
-			if(chargeTimer >= maxChargeTime)
-            {
-				characterController.EnterChargeState(1);
-				dashParticles.PlayDash(1);
-            }
-
-			isCharging = false;
-			chargeTimer = 0;
 		}
 	}	
 
