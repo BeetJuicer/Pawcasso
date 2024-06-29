038949d50a150bc56045fcf4d1dbb96c987962b8
Carl <94771147+BeetJuicer@users.noreply.github.com>
2024-06-29 17:58:20 +0800

    level enabling push

--- Assets/MoveLevel.cs
+++ Assets/MoveLevel.cs
@@ -8,13 +8,24 @@ public class MoveLevel : MonoBehaviour
 {
     [SerializeField] private Transform teleportTo;
     [SerializeField] private GameObject levelToDisable;
+    [SerializeField] private GameObject levelTOEnable;
+
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("Player"))
         {
+            if (levelTOEnable != null)
+                levelTOEnable.SetActive(true);
+            else
+                Debug.LogWarning("No level to enable. Are you sure?");
+
             other.GetComponent<KinematicCharacterMotor>().SetPosition(teleportTo.position);
-            levelToDisable.SetActive(false);
+
+            if (levelToDisable != null)
+                levelToDisable.SetActive(false);
+            else
+                Debug.LogWarning("No level disabled. Are you sure?");
         }
     }
 }
